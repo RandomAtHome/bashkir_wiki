bb5c045d25f819bde975ef45f3098db170532f99
RandomAtHome <random-s_home@list.ru>
2015-04-16 07:51:27 +0300

    Changed template code in webparser.py to actually working parser of given page

--- web_parser.py
+++ web_parser.py
@@ -1,8 +1,13 @@
 from bs4 import BeautifulSoup
 import urllib.request
 
-
-webpage = urllib.request.urlopen('http://en.wikipedia.org/wiki/Main_Page')
+webpage = urllib.request.urlopen('https://en.wikipedia.org/wiki/Multinational_corporation')
 soup = BeautifulSoup(webpage)
-for anchor in soup.find_all('a'):
-    print(anchor.get('href', '/'))
+for tag_refl in soup.find_all('ol'):
+    print((len(tag_refl.contents) - 1) // 2)
+    """ tag_refl.contents resembles a list of tags inside tag <ol>
+        there are N external references, each 
+        closed between 2 tags - <li> and </li>
+        and there is a closing tag </ol>
+        in summary we have 2 * N + 1 elements in tag_refl.contents, where N is number of ext. ref
+    """
