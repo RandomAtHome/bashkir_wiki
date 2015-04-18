c4d0520b4501d2bac729a3316f6cab38c6ecec1e
RandomAtHome <random-s_home@list.ru>
2015-04-18 14:02:01 +0300

    Added parsing of the results into HTML file via Jinja2

--- ReadMe_libs.txt
+++ ReadMe_libs.txt
@@ -1,3 +1,8 @@
 For now, I've imported 
-	BeautifulSoup 4.3.2
-	urllib 
+	side-packages
+	- BeautifulSoup 4.3.2
+	- Jinja2-2.7.3-py3.4
+	already existing
+	- urllib
+
+All side-packages were downloaded via pip.
--- web_parser.py
+++ web_parser.py
@@ -1,13 +1,54 @@
+from jinja2 import Template
 from bs4 import BeautifulSoup
 import urllib.request
 
-webpage = urllib.request.urlopen('https://en.wikipedia.org/wiki/Multinational_corporation')
+
+out_html = open("results.html", "w")
+results = []
+
+# start of reading Wiki
+url = 'https://en.wikipedia.org/wiki/Multinational_corporation'
+webpage = urllib.request.urlopen(url)
 soup = BeautifulSoup(webpage)
+
+name = soup.find('h1').string
+# gets the title of the article (not the webpage)
+
 for tag_refl in soup.find_all('ol'):
-    print((len(tag_refl.contents) - 1) // 2)
+    results.append((name, url, (len(tag_refl.contents) - 1) // 2))
     """ tag_refl.contents resembles a list of tags inside tag <ol>
-        there are N external references, each 
+        there are N external references, each
         closed between 2 tags - <li> and </li>
         and there is a closing tag </ol>
-        in summary we have 2 * N + 1 elements in tag_refl.contents, where N is number of ext. ref
+        in summary we have 2 * N + 1 elements in tag_refl.contents,
+        where N is number of ext. ref.
     """
+results.sort(key=lambda x: x[2])
+# end of reading Wiki
+
+# Jinja code interpretating results
+result_page = Template(u'''\
+<html>
+<head><title>Results of searching</title></head>
+<body>
+<table border>
+<tr>
+<th>Name of page</th>
+<th>URL of page</th>
+<th>Amount of external references</th>
+</tr>
+{% for result in item_list -%}
+<tr>
+   <td>{{ result[0] }}</td>
+   <td><a href="{{ result[1] }}">{{ result[1] }}</td>
+   <td align="middle">{{ result[2] }}</td>
+</tr>
+{% endfor %}
+</table>
+</body>
+</html>''')
+# Jinja code ended
+
+print(result_page.render(item_list=results), file=out_html)
+out_html.close()
+# creating output file and closing it
